48115bc801d331e5940ab5efc0992ae932f792ab
SamuelBarb0 <sambarbosa66@gmail.com>
2025-11-01 18:04:05 -0500

    Imagi XIV

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -39,7 +39,7 @@
 
             <!-- Right Icons -->
             <div class="flex items-center space-x-6">
-                <a href="{{ url('carrito') }}" class="text-dark-turquoise hover:scale-110 transition-transform">
+                <a href="{{ url('pruebas/carrito') }}" class="text-dark-turquoise hover:scale-110 transition-transform">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
                     </svg>
@@ -72,10 +72,10 @@
         <!-- Mobile Menu -->
         <div id="mobileMenu" class="hidden lg:hidden mt-4 pb-4 border-t border-gray-200 pt-4">
             <ul class="space-y-3 text-base">
-                <li><a href="{{ url('personalizados') }}" class="block text-gray-700 hover:text-dark-turquoise transition uppercase">Personalizados</a></li>
-                <li><a href="{{ url('colecciones') }}" class="block text-gray-700 hover:text-dark-turquoise transition uppercase">Colecciones</a></li>
-                <li><a href="{{ url('mayoristas') }}" class="block text-gray-700 hover:text-dark-turquoise transition uppercase">Mayoristas</a></li>
-                <li><a href="{{ url('contacto') }}" class="block text-gray-700 hover:text-dark-turquoise transition uppercase">Contacto</a></li>
+                <li><a href="{{ url('pruebas/personalizados') }}" class="block text-gray-700 hover:text-dark-turquoise transition uppercase">Personalizados</a></li>
+                <li><a href="{{ url('pruebas/colecciones') }}" class="block text-gray-700 hover:text-dark-turquoise transition uppercase">Colecciones</a></li>
+                <li><a href="{{ url('pruebas/mayoristas') }}" class="block text-gray-700 hover:text-dark-turquoise transition uppercase">Mayoristas</a></li>
+                <li><a href="{{ url('pruebas/contacto') }}" class="block text-gray-700 hover:text-dark-turquoise transition uppercase">Contacto</a></li>
             </ul>
         </div>
     </div>

d31d6b770fd7c71d1783293347252c7554089f0e
SamuelBarb0 <sambarbosa66@gmail.com>
2025-11-11 16:41:32 -0500

    Imagi XXXVI

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -52,7 +52,14 @@
                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
                     </svg>
                     @php
-                        $cartCount = session('cart') ? array_sum(array_column(session('cart'), 'quantity')) : 0;
+                        use App\Models\Cart;
+                        $cart = null;
+                        if (Auth::check()) {
+                            $cart = Cart::where('user_id', Auth::id())->first();
+                        } else {
+                            $cart = Cart::where('session_id', session()->getId())->first();
+                        }
+                        $cartCount = $cart ? $cart->getTotalItems() : 0;
                     @endphp
                     <span data-cart-count class="absolute -top-2 -right-2 bg-gray-orange text-white text-xs font-bold rounded-full h-5 w-5 flex items-center justify-center {{ $cartCount > 0 ? '' : 'hidden' }}">
                         {{ $cartCount }}

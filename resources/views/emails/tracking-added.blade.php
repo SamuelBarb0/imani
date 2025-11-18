06463cfd2841e68816473f58dc595c9b62e5cba5
SamuelBarb0 <sambarbosa66@gmail.com>
2025-11-18 17:34:48 -0500

    Imagi XXXXXX

--- resources/views/emails/tracking-added.blade.php
+++ resources/views/emails/tracking-added.blade.php
@@ -24,13 +24,17 @@
     <p style="margin: 5px 0 5px 0; color: #333; line-height: 1.6;"><strong>Número de seguimiento:</strong> {{ $order->tracking_number }}</p>
 
     @if($order->tracking_url)
-    <p style="margin: 5px 0 20px 0; color: #333; line-height: 1.6;">
-        <strong>Rastrea tu pedido:</strong>
+    <p style="margin: 5px 0 5px 0; color: #333; line-height: 1.6;">
+        <strong>Link de seguimiento:</strong>
         <a href="{{ $order->tracking_url }}" style="color: #12463c; text-decoration: underline;" target="_blank">
-            Ver seguimiento detallado
+            {{ $order->tracking_url }}
         </a>
     </p>
     @endif
+
+    <p style="margin: 5px 0 20px 0; color: #666; font-size: 13px; line-height: 1.6;">
+        <em>Ingresa el número de seguimiento en la página del courier para rastrear tu pedido.</em>
+    </p>
 
     <p style="margin: 0 0 15px 0; color: #333; line-height: 1.6;">Esperamos que disfrutes mucho tus imanes y que llenen de alegría tu espacio.</p>
 
